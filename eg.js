2f029bb92e5f010a31e00bf29142ba2cb3ed4b8e
mowabanga <itsmundia@gmail.com>
2023-06-18 10:13:06 +0300

    Space X appends

--- eg.js
+++
@@ -1,6 +0,0 @@
-const burger = document.getElementsByClassName("burger")[0];
-const navBarLinks = document.getElementsByClassName('navbar-links')[0];
-
-burger.addEventListener('click', () => {
-    navBarLinks.classList.toggle('active');
-})
